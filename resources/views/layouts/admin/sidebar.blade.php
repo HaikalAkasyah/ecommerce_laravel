9ed98a88deb91ef9448baebd559d6c6c62eba5c9
HaikalAkasyah <sarhesa726@gmail.com>
2024-11-14 22:16:09 +0700

    commit kedua

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -4,13 +4,18 @@
             <a href="#">Teknik Informatika | KSI</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="#">KSI</a>
+            <a href="#">RPL</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Menu</li>
             <li class="{{ Route::is('admin.dashboard') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('admin.dashboard') }}">
                     <i class="fas fa-home"></i> <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="{{ Route::is('admin.product') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('admin.product') }}">
+                    <i class="fas fa-box"></i> <span>Produk</span>
                 </a>
             </li>
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -2,35 +2,51 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\User\UserController;
 
 // Guest Route
-Route::group(['middleware' => 'guest'], function () {
+// Routes khusus untuk pengguna yang belum login (guest)
+Route::group(['middleware' => 'guest'], function() {
+
+    // Route untuk halaman welcome
     Route::get('/', function () {
         return view('welcome');
     });
 
-    Route::post('/post-login', [AuthController::class, 'login'])
-        ->middleware('guest');
+    // Route untuk halaman registrasi
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+    // Route untuk mengirim data registrasi
+    Route::post('/post-register', [AuthController::class, 'post_register'])->name('post.register');
+
+    // Route untuk proses login
+    Route::post('/post-login', [AuthController::class, 'login']);
 });
 
 // Admin Route
-Route::group(['middleware' => 'admin'], function () {
-    Route::get('/admin', function () {
-        return view('pages.admin.index');
-    })->name('admin.dashboard');
+// Routes khusus untuk admin dengan middleware 'admin'
+Route::group(['middleware' => 'admin'], function() {
 
-    Route::get('/admin-logout', [AuthController::class, 'admin_logout'])
-        ->name('admin.logout')
-        ->middleware('admin');
+    // Route untuk halaman dashboard admin
+    Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+
+    // Product Route
+    // Route untuk menampilkan halaman produk di admin
+    Route::get('/product', [ProductController::class, 'index'])->name('admin.product');
+
+    // Route untuk logout admin
+    Route::get('/admin-logout', [AuthController::class, 'admin_logout'])->name('admin.logout');
 });
 
 // User Route
-Route::group(['middleware' => 'web'], function () {
-    Route::get('/user', function () {
-        return view('pages.user.index');
-    })->name('user.dashboard');
+// Routes khusus untuk user yang telah login dengan middleware 'web'
+Route::group(['middleware' => 'web'], function() {
 
-    Route::get('/user-logout', [AuthController::class, 'userLogout'])
-        ->name('user.logout')
-        ->middleware('web');
+    // Route untuk halaman dashboard user
+    Route::get('/user', [UserController::class, 'index'])->name('user.dashboard');
+
+    // Route untuk logout user
+    Route::get('/user-logout', [AuthController::class, 'user_logout'])->name('user.logout');
 });
